113a3c405039aa23a0f4987fa5ee620ba1dec7e7
Hong Yan <homer.yan@gmail.com>
2016-12-01 15:43:45 -0700

    New example with multipul crawlers.

--- example/index.js
+++ example/index.js
@@ -1,18 +1,36 @@
 const Crawler = require('../lib/dhtcrawler');
+const InfohashQueue = require('../lib/infohashqueue');
+const recentInfohashes = new InfohashQueue(1024);
 
 const CONSOLE_NC = '\033[0m';
+const CONSOLE_RED = '\033[0;31m';
 const CONSOLE_GREEN = '\033[0;32m';
 const CONSOLE_BROWN = '\033[0;33m';
 const CONSOLE_BLUE = '\033[0;34m';
+const CONSOLE_PURPLE = '\033[0;35m';
+const CONSOLE_CYAN = '\033[0;36m';
+const COLORS = [CONSOLE_GREEN, CONSOLE_BROWN, CONSOLE_BLUE, CONSOLE_PURPLE, CONSOLE_CYAN];
 
-const crawler1 = Crawler.createCrawler({address: '0.0.0.0', port: 6881, kbucketSize: 384, name: 'crawler1'});
-crawler1.on('infohash', (infohash, peerAddress) => 
-  console.log(`${CONSOLE_GREEN}magnet:?xt=urn:btih:${infohash}${CONSOLE_NC} from ${peerAddress.address}:${peerAddress.port}`));
+let crawlerCounter = 0;
+const BASE_PORT = 6881;
+const KBUCKET_SIZE = 128;
 
-const crawler2 = Crawler.createCrawler({address: '0.0.0.0', port: 6882, kbucketSize: 384, name: 'crawler2'});
-crawler2.on('infohash', (infohash, peerAddress) => 
-  console.log(`${CONSOLE_BROWN}magnet:?xt=urn:btih:${infohash}${CONSOLE_NC} from ${peerAddress.address}:${peerAddress.port}`));
+startCrawl(2);
 
-const crawler3 = Crawler.createCrawler({address: '0.0.0.0', port: 6883, kbucketSize: 384, name: 'crawler3'});
-crawler3.on('infohash', (infohash, peerAddress) => 
-  console.log(`${CONSOLE_BLUE}magnet:?xt=urn:btih:${infohash}${CONSOLE_NC} from ${peerAddress.address}:${peerAddress.port}`));
+function startCrawl(numOfCrawlers) {
+  const noc = numOfCrawlers >= 5 ? 5 : numOfCrawlers;
+  const crawlers = [];
+  for (let index = 0; index < noc; ++index) {
+    let crawler = Crawler.createCrawler({
+      address: '0.0.0.0',
+      port: BASE_PORT + index, 
+      kbucketSize: KBUCKET_SIZE,
+      name: `crawler - ${index+1}`});
+    crawler.on('infohash', (infohash, peerId, peerAddress) => {
+      if (!recentInfohashes.enqueue(infohash)) return;
+      console.log(`${COLORS[index]}magnet:?xt=urn:btih:${infohash}${CONSOLE_NC} ` + 
+        `${++crawlerCounter} from ${peerAddress.address}:${peerAddress.port}`);
+    });
+    crawlers.push(crawler);
+  }
+}
